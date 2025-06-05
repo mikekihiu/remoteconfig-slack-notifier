439cd498857ea0c579fb3818dc6f7f7e1e365540
Mike Kihiu <mikekihiu@icloud.com>
2025-06-05 11:23:56 +0300

    Working trigger

--- functions/index.js
+++ functions/index.js
@@ -1,75 +1,68 @@
-const admin = require("firebase-admin");
-const jsonDiff = require("json-diff");
-const logger = require("firebase-functions/logger");
+"use strict";
 
+// [START all]
+// [START import]
+// The Cloud Functions for Firebase SDK to set up triggers and logging.
 const {onConfigUpdated} = require("firebase-functions/v2/remoteConfig");
-// const {defineSecret} = require("firebase-functions/params");
-const {getRemoteConfig} = require("firebase-admin/remote-config");
+const logger = require("firebase-functions/logger");
+// The Firebase Admin SDK to obtain access tokens.
+const admin = require("firebase-admin");
+const app = admin.initializeApp();
+const fetch = require("node-fetch");
+const jsonDiff = require("json-diff");
 
-const app = admin.initializeApp({credential: admin.credential.applicationDefault()});
+const {IncomingWebhook} = require("@slack/webhook");
+const SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
+const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
+// [END import]
 
-// Register the private Slack incoming webhook secret
-const slackIncomingWebhook = "https://hooks.slack.com/services/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";
-
-// Register for Remote Config updates
-// @see https://firebase.google.com/docs/reference/functions/2nd-gen/node/firebase-functions.remoteconfig.configupdatedata
-exports.remoteConfigUpdate = onConfigUpdated({secrets: [slackIncomingWebhook]}, async (event) => {
-  logger.log("onConfigUpdated", event);
-  const rc = getRemoteConfig(app);
-
+// [START showconfigdiff]
+exports.remoteConfigUpdate = onConfigUpdated(async (event) => {
   try {
-    // Fetch Remote Config templates
-    // @see https://firebase.google.com/docs/reference/admin/node/firebase-admin.remote-config.remoteconfigtemplate.md#remoteconfigtemplate_interface
-    const current = await rc.getTemplateAtVersion(event.data.versionNumber);
-    const previous = await rc.getTemplateAtVersion((event.data.versionNumber - 1));
+    // Obtain the access token from the Admin SDK
+    const accessTokenObj = await admin.credential.applicationDefault()
+        .getAccessToken();
+    const accessToken = accessTokenObj.access_token;
 
-    // Delete `etagInternal` and `version` properties to avoid polluting the final diff
-    [previous, current].forEach((template) => {
-      delete template.etagInternal;
-      delete template.version;
-    });
-
-    // Diff Remote Config templates
-    const diff = jsonDiff.diffString(previous, current, {maxElisions: 0});
-    logger.log("diff", diff);
-
-    // Build Slack payload https://api.slack.com/reference/block-kit/blocks
-    const payload = {
-      blocks: [
-        // Header: image | author | project
+    // Get the version number from the event object
+    const remoteConfigApi = "https://firebaseremoteconfig.googleapis.com/v1/" +
+        `projects/${app.options.projectId}/remoteConfig`;
+    const currentVersion = event.data.versionNumber;
+    const prevVersion = currentVersion - 1;
+    const templatePromises = [];
+    templatePromises.push(fetch(
+        remoteConfigApi,
         {
-          "type": "context",
-          "elements": [
-            ...(event.data.updateUser?.imageUrl ? [{"type": "image", "image_url": event.data.updateUser?.imageUrl, "alt_text": "author image"}] : []),
-            {"type": "mrkdwn", "text": `*Author*: ${event.data.updateUser?.name || event.data.updateUser?.email || "unknown"}`},
-            {"type": "mrkdwn", "text": `*Project*: <https://console.firebase.google.com/project/${event.project}/config|${event.project}>`},
-          ],
+          method: "POST",
+          body: new URLSearchParams([["versionNumber", currentVersion + ""]]),
+          headers: {Authorization: "Bearer " + accessToken},
         },
-        // Details: type | origin | version
+    ));
+    templatePromises.push(fetch(
+        remoteConfigApi,
         {
-          "type": "context",
-          "elements": [
-            {"type": "mrkdwn", "text": `*Type*: ${event.data.updateType}`},
-            {"type": "mrkdwn", "text": `*Origin*: ${event.data.updateOrigin}`},
-            {"type": "mrkdwn", "text": `*Version*: ${event.data.versionNumber}`},
-          ],
+          method: "POST",
+          body: new URLSearchParams([["versionNumber", prevVersion + ""]]),
+          headers: {Authorization: "Bearer " + accessToken},
         },
-        // Description
-        ...(event.data.description ? [{"type": "context", "elements": [{"type": "mrkdwn", "text": `*Description*: ${event.data.description}`}]}] : []),
-        // Diff
-        ...(diff ? [{"type": "rich_text", "elements": [{"type": "rich_text_preformatted", "elements": [{"type": "text", "text": diff}]}]}] : []),
-      ],
+    ));
+
+    // Get the templates
+    const responses = await Promise.all(templatePromises);
+    const results = responses.map((r) => r.json());
+    const currentTemplate = results[0];
+    const previousTemplate = results[1];
+    // Figure out the differences of the templates
+    const diff = jsonDiff.diffString(previousTemplate, currentTemplate);
+    // Log the difference
+    logger.log(diff);
+
+    const message = {
+      text: `:rocket: Firebase Remote Config updated ${diff}`,
     };
-    logger.log("payload", payload);
-
-    // Send Slack message
-    const result = await fetch(slackIncomingWebhook.value(), {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers: {"Content-Type": "application/json"},
-    });
-    logger.log(result);
+    webhook.send(message);
   } catch (error) {
     logger.error(error);
   }
 });
+
